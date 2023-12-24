df6ab41cb62533fa1426e9461d22a778d1d09ef2
onsclom <onsclom@onsclom.net>
2023-12-24 12:58:26 -0800

    Add dead letter animations

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,25 @@ type App = ReturnType<typeof create>;
 const fontKey = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const text = "the quick brown fox jumps over the lazy dog";
 
+type DeadLetter = {
+  char: string;
+
+  x: number;
+  y: number;
+
+  dx: number;
+  dy: number;
+
+  angleDelta: number;
+  angle: number;
+};
+
 export const create = () => ({
   curPos: 0,
   ms: 0,
   state: "waiting" as "waiting" | "playing" | "done",
   lastTime: performance.now(),
+  deadLetters: [] as DeadLetter[],
 });
 
 export function update(app: App) {
@@ -20,10 +34,20 @@ export function update(app: App) {
     app.ms = 0;
     app.state = "waiting";
     Input.keyQueue.splice(0, Input.keyQueue.length);
+    app.deadLetters.splice(0, app.deadLetters.length);
   }
 
   while (Input.keyQueue.length > 0) {
     if (Input.keyQueue[0] === text[app.curPos]) {
+      app.deadLetters.push({
+        char: Input.keyQueue[0],
+        x: 0,
+        y: 0,
+        dx: Math.random() * 5 - 2.5,
+        dy: Math.random() * -4 - 4,
+        angleDelta: Math.random() * 0.1 - 0.05,
+        angle: 0,
+      });
       app.curPos += 1;
       if (app.state === "waiting") app.state = "playing";
       else if (app.curPos === text.length) app.state = "done";
@@ -31,8 +55,16 @@ export function update(app: App) {
     Input.keyQueue.shift();
   }
 
+  const deltaTime = (performance.now() - app.lastTime) * 0.1;
   if (app.state === "playing") app.ms += performance.now() - app.lastTime;
   app.lastTime = performance.now();
+
+  for (const deadLetter of app.deadLetters) {
+    deadLetter.x += deadLetter.dx * deltaTime;
+    deadLetter.y += deadLetter.dy * deltaTime;
+    deadLetter.dy += 0.1 * deltaTime;
+    deadLetter.angle += deadLetter.angleDelta * deltaTime;
+  }
 }
 
 export function draw(app: App, ctx: CanvasRenderingContext2D) {
@@ -45,11 +77,29 @@ export function draw(app: App, ctx: CanvasRenderingContext2D) {
   const fontHeight = fontWidth * 2;
   const screen = Canvas.canvasRect();
 
+  const centeredChar = {
+    x: screen.width / 2 - fontWidth / 2,
+    y: screen.height / 2 - fontHeight / 2,
+  };
+
+  ctx.save();
+  ctx.strokeStyle = "#555";
+  ctx.translate(centeredChar.x, centeredChar.y);
+  for (const deadLetter of app.deadLetters) {
+    const charLines = Text.getCharLines(deadLetter.char);
+    ctx.save();
+    ctx.translate(deadLetter.x, deadLetter.y);
+
+    ctx.translate(fontWidth / 2, fontHeight / 2);
+    ctx.rotate(deadLetter.angle);
+    ctx.translate(-fontWidth / 2, -fontHeight / 2);
+
+    Text.drawChar(ctx, charLines, 0, 0, fontWidth, 5);
+    ctx.restore();
+  }
+
+  ctx.restore();
   if (app.curPos < text.length) {
-    const centeredChar = {
-      x: screen.width / 2 - fontWidth / 2,
-      y: screen.height / 2 - fontHeight / 2,
-    };
     ctx.beginPath();
     ctx.moveTo(centeredChar.x, centeredChar.y + fontHeight * 1.5);
     ctx.lineTo(
