4f11f697670a55a30bd5e9bd7150514fadbedf7b
Austin Merrick <austinmerrick@onsclom.net>
2025-01-19 20:35:00 -0800

    MAKE BETTER

--- src/app.ts
+++ src/app.ts
@@ -16,34 +16,34 @@ type DeadLetter = {
   dy: number;
   angleDelta: number;
   angle: number;
+  timeDead: number;
 };
 
-const colors = [
-  ["#000", "#0f0"],
-  ["#222323", "#f0f6f0"],
-  ["#004c3d", "#ffeaf9"],
-  ["#292b30", "#cfab4a"],
-  ["#323c39", "#d3c9a1"],
-  ["#0a2e44", "#fcffcc"],
-  ["#40318e", "#88d7de"],
-];
+const colorAmt = 2;
+const getColor = (i: number) => {
+  const hue = (i * 360 + performance.now() * 0.025) / colorAmt;
+  return `hsl(${hue}, 90%, 65%)`;
+};
 
 export const create = () => ({
   curPos: 0,
+  animatedPos: 0,
   ms: 0,
   state: "waiting" as "waiting" | "playing" | "done",
   lastTime: performance.now(),
   deadLetters: [] as DeadLetter[],
+  timeDead: 0,
   freq: 440,
   colorIndex: 0,
+  shakeStrength: 0,
+  highScore: localStorage.getItem("highScore")
+    ? Number(localStorage.getItem("highScore"))
+    : 0,
 });
 
 export function update(app: App) {
-  if (Input.justPressed.has("Shift")) {
-    app.colorIndex += 1;
-  }
-
   if (Input.justPressed.has("Enter")) {
+    app.shakeStrength = 40;
     app.curPos = 0;
     app.ms = 0;
     app.state = "waiting";
@@ -52,16 +52,20 @@ export function update(app: App) {
     Sound.resetFreq();
   }
 
+  const shakePerKey = 5;
   while (Input.keyQueue.length > 0) {
     if (Input.keyQueue[0] === text[app.curPos]) {
+      app.shakeStrength += shakePerKey;
+      const animatedDiff = (app.animatedPos - app.curPos) * 32;
       app.deadLetters.push({
         char: Input.keyQueue[0],
-        x: 0,
+        x: 0 - animatedDiff + 32,
         y: 0,
-        dx: Math.random() * -1 - 1,
-        dy: 0,
+        dx: Math.random() * -1 - 3,
+        dy: Math.random() * -4 - 1,
         angleDelta: Math.random() * -0.05,
         angle: 0,
+        timeDead: 0,
       });
       app.curPos += 1;
       Sound.playGoodSound();
@@ -71,6 +75,7 @@ export function update(app: App) {
       else if (app.curPos === text.length) app.state = "done";
     } else {
       Sound.playBadSound();
+      app.shakeStrength += shakePerKey;
     }
     Input.keyQueue.shift();
   }
@@ -80,6 +85,7 @@ export function update(app: App) {
   app.lastTime = performance.now();
 
   for (const deadLetter of app.deadLetters) {
+    deadLetter.timeDead += deltaTime;
     deadLetter.x += deadLetter.dx * deltaTime;
     deadLetter.y += deadLetter.dy * deltaTime;
     deadLetter.dy += 0.1 * deltaTime;
@@ -87,18 +93,17 @@ export function update(app: App) {
   }
 }
 
-function curColors(app: App) {
-  return colors[app.colorIndex % colors.length];
-}
-
 export function draw(app: App, ctx: CanvasRenderingContext2D) {
+  const fontWidth = 32;
+  app.animatedPos += (app.curPos - app.animatedPos) * 0.1;
+  const animatedDiff = (app.animatedPos - app.curPos) * fontWidth;
+
   const screen = Canvas.canvasRect();
-  ctx.fillStyle = curColors(app)[0];
+  ctx.fillStyle = getColor(0);
   ctx.fillRect(0, 0, screen.width, screen.height);
-  ctx.strokeStyle = curColors(app)[1];
+  ctx.strokeStyle = getColor(1);
   ctx.lineCap = "round";
-  ctx.lineWidth = 1;
-  const fontWidth = 16;
+  ctx.lineWidth = 4;
   const fontHeight = fontWidth * 2;
 
   const centeredChar = {
@@ -106,11 +111,25 @@ export function draw(app: App, ctx: CanvasRenderingContext2D) {
     y: screen.height / 2 - fontHeight / 2,
   };
 
+  // screenshake
+  app.shakeStrength *= 0.9;
+  // rotate from center
+  ctx.translate(screen.width / 2, screen.height / 2);
+  ctx.rotate(Math.sin(performance.now() * 0.01) * app.shakeStrength * 0.0025);
+  ctx.translate(-screen.width / 2, -screen.height / 2);
+  ctx.translate(
+    Math.cos(performance.now() * 0.024) * app.shakeStrength,
+    Math.sin(performance.now() * 0.025) * app.shakeStrength,
+  );
+
   ctx.save();
   ctx.translate(centeredChar.x - fontWidth, centeredChar.y);
+
   for (const deadLetter of app.deadLetters) {
     const charLines = Text.getCharLines(deadLetter.char);
     ctx.save();
+    const alpha = Math.max(0, 1 - deadLetter.timeDead * 0.008);
+    ctx.globalAlpha = alpha;
     ctx.translate(deadLetter.x, deadLetter.y);
 
     ctx.translate(fontWidth / 2, fontHeight / 2);
@@ -124,18 +143,28 @@ export function draw(app: App, ctx: CanvasRenderingContext2D) {
 
   if (app.curPos < text.length) {
     ctx.beginPath();
+    console.log("drawng text");
     ctx.moveTo(centeredChar.x, centeredChar.y + fontHeight * 1.5);
     ctx.lineTo(
       centeredChar.x + fontWidth / 2,
-      centeredChar.y + fontHeight * 1.2
+      centeredChar.y + fontHeight * 1.2,
     );
     ctx.lineTo(centeredChar.x + fontWidth, centeredChar.y + fontHeight * 1.5);
     ctx.stroke();
     ctx.save();
     ctx.translate(centeredChar.x, centeredChar.y);
+
     for (const [i, char] of text.slice(app.curPos).split("").entries()) {
       const charLines = Text.getCharLines(char);
-      Text.drawChar(ctx, charLines, i * fontWidth, 0, fontWidth, 5);
+      console.log(animatedDiff);
+      Text.drawChar(
+        ctx,
+        charLines,
+        i * fontWidth - animatedDiff,
+        0,
+        fontWidth,
+        5,
+      );
     }
     ctx.restore();
 
@@ -153,24 +182,22 @@ export function draw(app: App, ctx: CanvasRenderingContext2D) {
         wpmXPos + i * fontWidth,
         fontWidth / 2,
         fontWidth,
-        5
+        5,
       );
     }
   } else {
     ctx.save();
     ctx.globalAlpha = 0.65;
-    ctx.fillStyle = curColors(app)[0];
+    ctx.fillStyle = getColor(0);
     ctx.fillRect(0, 0, screen.width, screen.height);
     ctx.restore();
 
     const wpm = Math.round((text.split(" ").length / app.ms) * 60000);
-    const lines = [
-      `${wpm} wpm`,
-      ``,
-      `enter to retry`,
-      ``,
-      `shift to change color`,
-    ];
+    if (wpm > app.highScore) {
+      app.highScore = wpm;
+      localStorage.setItem("highScore", wpm.toString());
+    }
+    const lines = [`${wpm} wpm`, ``, `enter to retry`];
     for (const [i, line] of lines.entries()) {
       const centeredLine = {
         x: screen.width / 2 - (fontWidth * line.length) / 2,
@@ -185,6 +212,38 @@ export function draw(app: App, ctx: CanvasRenderingContext2D) {
         Text.drawChar(ctx, charLines, i * fontWidth, 0, fontWidth, 5);
       }
       ctx.restore();
+    }
+
+    if (wpm >= app.highScore) {
+      const highScoreText = `new high score`;
+      const highScoreXPos =
+        screen.width / 2 - (highScoreText.length * fontWidth) / 2;
+      for (const [i, char] of highScoreText.split("").entries()) {
+        const charLines = Text.getCharLines(char);
+        Text.drawChar(
+          ctx,
+          charLines,
+          highScoreXPos + i * fontWidth,
+          fontWidth / 2 + Math.sin(performance.now() * 0.01 + i * 0.2) * 10,
+          fontWidth,
+          5,
+        );
+      }
+    } else {
+      const highScoreText = `high score ${app.highScore} wpm`;
+      const highScoreXPos =
+        screen.width / 2 - (highScoreText.length * fontWidth) / 2;
+      for (const [i, char] of highScoreText.split("").entries()) {
+        const charLines = Text.getCharLines(char);
+        Text.drawChar(
+          ctx,
+          charLines,
+          highScoreXPos + i * fontWidth,
+          fontWidth / 2,
+          fontWidth,
+          5,
+        );
+      }
     }
   }
 }
